b2a2e2ddb3f086a85982000ad9538721906d0ec8
Chrisjohanes <95056992+Chrisjohanes@users.noreply.github.com>
2026-03-11 14:08:37 +0700

    fix homepage

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TicketController;
 
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');
 });
 
 Route::get('/dashboard', [TicketController::class,'dashboard'])
